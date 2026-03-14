54f8db2441c0fbdfa758b67fba0d0ae095571e48
hzj5158888 <www1693303798@163.com>
2026-03-14 18:35:28 +0800

    optimization zram

--- rootdir/bin/init.kernel.post_boot.sh
+++ rootdir/bin/init.kernel.post_boot.sh
@@ -43,19 +43,19 @@ function configure_zram_parameters() {
 	let RamSizeGB="( $MemTotal / 1048576 ) + 1"
 	diskSizeUnit=M
 	if [ $RamSizeGB -le 2 ]; then
-		let zRamSizeMB="( $RamSizeGB * 1024 ) * 3 / 4"
+	  	let zRamSizeMB="( $RamSizeGB * 1024 ) * 3 / 4"
 	else
-		let zRamSizeMB="( $RamSizeGB * 1024 ) / 2"
+	  	let zRamSizeMB="( ($RamSizeGB - 4) * 1024 )"
 	fi
 
 	# use MB avoid 32 bit overflow
-	if [ $zRamSizeMB -gt 4096 ]; then
-		let zRamSizeMB=4096
+	if [ $zRamSizeMB -gt 8192 ]; then
+		let zRamSizeMB=8192
 	fi
 
-	if [ "$low_ram" == "true" ]; then
-		echo lz4 > /sys/block/zram0/comp_algorithm
-	fi
+	#if [ "$low_ram" == "true" ]; then
+	echo lz4 > /sys/block/zram0/comp_algorithm
+	#fi
 
 	if [ -f /sys/block/zram0/disksize ]; then
 		if [ -f /sys/block/zram0/use_dedup ]; then
