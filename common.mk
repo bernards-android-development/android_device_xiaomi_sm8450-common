3cee2e49d24e42ce7c38821ef7b96d78509221a8
hzj5158888 <www1693303798@163.com>
2026-03-22 13:36:52 +0800

    Power: move powerhint.json to device path

--- common.mk
+++ common.mk
@@ -321,9 +321,6 @@ PRODUCT_PACKAGES += \
     android.hardware.power-service.lineage-libperfmgr \
     libqti-perfd-client
 
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
-
 # Powershare
 $(call soong_config_set,lineage_powershare,powershare_path,/sys/class/qcom-battery/reverse_chg_mode)
 
