4cc0503b8dce8da64c4137bf949d39359c1a994a
hzj5158888 <www1693303798@163.com>
2026-03-25 17:36:19 +0800

    init: tune for mglru and cpuset

--- rootdir/bin/init.kernel.post_boot-cape.sh
+++ rootdir/bin/init.kernel.post_boot-cape.sh
@@ -55,8 +55,8 @@ echo 15 15 15 15 15 15 15 15 > /proc/sys/walt/sched_util_busy_hyst_cpu_util
 echo 325 > /proc/sys/walt/walt_low_latency_task_threshold
 
 # cpuset parameters
-echo 1-3 > /dev/cpuset/background/cpus
-echo 0-3 > /dev/cpuset/system-background/cpus
+echo 0-2 > /dev/cpuset/background/cpus
+echo 0-2 > /dev/cpuset/system-background/cpus
 
 # Turn off scheduler boost at the end
 echo 0 > /proc/sys/walt/sched_boost
