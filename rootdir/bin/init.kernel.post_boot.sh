c5fb01ed0d15fb53dce1c58f12fdeb77fe6bfaf0
hzj5158888 <www1693303798@163.com>
2026-04-29 11:43:55 +0800

    sm8450-common: disable prefetch

--- rootdir/bin/init.kernel.post_boot.sh
+++ rootdir/bin/init.kernel.post_boot.sh
@@ -130,6 +130,8 @@ function configure_memory_parameters() {
 	configure_zram_parameters
 	configure_read_ahead_kb_values
 	echo 100 > /proc/sys/vm/swappiness
+	echo 0 > /proc/sys/vm/page-cluster
+	echo "false" > /sys/kernel/mm/swap/vma_ra_enabled
 
 	# Disable periodic kcompactd wakeups. We do not use THP, so having many
 	# huge pages is not as necessary.
