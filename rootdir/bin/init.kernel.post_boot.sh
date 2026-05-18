7e8d6874429091f6e33d219bca2f097c365cc2e9
root <www1693303798@163.com>
2026-05-18 16:27:41 +0800

    sm8450-common: optimize memory params

--- rootdir/bin/init.kernel.post_boot.sh
+++ rootdir/bin/init.kernel.post_boot.sh
@@ -151,6 +151,9 @@ function configure_memory_parameters() {
 	# Set the min_free_kbytes to standard kernel value
 	if [ $RamSizeGB -ge 8 ]; then
 		echo 11584 > /proc/sys/vm/min_free_kbytes
+		echo 100 > /proc/sys/vm/watermark_scale_factor
+		let extraFree="( $MemTotal / 100 )"
+		echo $extraFree > /proc/sys/vm/extra_free_kbytes
 	elif [ $RamSizeGB -ge 4 ]; then
 		echo 8192 > /proc/sys/vm/min_free_kbytes
 	elif [ $RamSizeGB -ge 2 ]; then
